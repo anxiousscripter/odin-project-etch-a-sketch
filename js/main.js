6c2df207deaa04ac51e282ae79f0ee6310834188
anxiousscripter <179371895+anxiousscripter@users.noreply.github.com>
2026-08-14 12:22:20 +0300

    New Feature: Create sketch grid
    Includes functionality and styling to create a sketch grid with any
    specified number of rows and columns.

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const wrapper = document.querySelector('.wrapper');
+
+const sketchGrid = document.createElement('div');
+sketchGrid.classList.add('sketch-grid');
+wrapper.appendChild(sketchGrid);
+
+function createCells(rows, columns) {
+	for (let i = 1; i <= rows; i++) {
+		const gridRow = document.createElement('div');
+		gridRow.classList.add('grid-row');
+
+		for (let j = 1; j <= columns; j++) {
+			const cell = document.createElement('div');
+			cell.classList.add('grid-cell');
+			gridRow.appendChild(cell);
+		}
+		sketchGrid.appendChild(gridRow);
+	}
+}
+
+createCells(16, 16);
